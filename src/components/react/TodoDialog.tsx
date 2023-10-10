09c3942f44a845236d35f2c3219688f93fdc627b
Ed Johnson-Williams <git@johnsonwilliams.co.uk>
2023-10-10 12:55:08 +0100

    get dialog to work with escape properly

--- src/components/react/TodoDialog.tsx
+++ src/components/react/TodoDialog.tsx
@@ -1,164 +1,183 @@
-import { type RefObject, type FC, useState, useEffect, type FormEvent } from "react";
-import { formatDistanceToNow } from "date-fns"
+import {
+  type RefObject,
+  type FC,
+  useState,
+  useEffect,
+  type FormEvent,
+} from "react";
+import { formatDistanceToNow } from "date-fns";
 import { enGB } from "date-fns/locale";
 import { updateTodo, $filterType } from "../../stores/store";
 import type { Todo } from "../../types/TodoType";
-import { HardDeleteSingleDeletedTodoButton, MarkAsDoneButton, MoveToBacklogButton, MoveToTodayButton, SoftDeleteTodoButton } from "./Buttons";
+import {
+  HardDeleteSingleDeletedTodoButton,
+  MarkAsDoneButton,
+  MoveToBacklogButton,
+  MoveToTodayButton,
+  SoftDeleteTodoButton,
+} from "./Buttons";
 import converter from "number-to-words";
-import { useStore } from "@nanostores/react";
 import { titleCase } from "title-case";
-
+import { useStore } from "@nanostores/react";
 
 type TodoDialogProps = {
-    todo: Todo | null;
-    onClose: () => void;
-    dialogRef: RefObject<HTMLDialogElement>;
+  todo: Todo | null;
+  onClose: () => void;
+  dialogRef: RefObject<HTMLDialogElement>;
 };
 
+export const TodoDialog: FC<TodoDialogProps> = ({
+  todo,
+  onClose,
+  dialogRef,
+}) => {
+  if (!todo) return null;
 
-export const TodoDialog: FC<TodoDialogProps> = ({ todo, onClose, dialogRef }) => {
-    if (!todo) return null;
+  const [showDescriptionEditor, setShowDescriptionEditor] = useState(false);
+  const [todoDescription, setTodoDescription] = useState(todo.description);
 
-    const [showDescriptionEditor, setShowDescriptionEditor] = useState(false);
-    const [todoDescription, setTodoDescription] = useState(todo.description);
+  const filterType = useStore($filterType);
 
-    const filterType = useStore($filterType);
+  useEffect(() => {
+    const dialogElement = dialogRef.current;
 
-    useEffect(() => {
-        if (todo && dialogRef.current) {
-            dialogRef.current.showModal();
-        } else if (!todo && dialogRef.current) {
-            dialogRef.current.close();
-        }
-    }, [todo]);
+    function handleCancelEvent() {
+      onClose();
+    }
 
+    if (todo && dialogElement) {
+      dialogElement.showModal();
+      dialogElement.addEventListener("cancel", handleCancelEvent);
+    } else if (!todo && dialogElement) {
+      dialogElement.close();
+      dialogElement.removeEventListener("cancel", handleCancelEvent);
+    }
 
-    const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        updateTodo({ ...todo, description: todoDescription });
-        setShowDescriptionEditor(false);
-        // onClose();
+    return () => {
+      dialogElement?.removeEventListener("cancel", handleCancelEvent);
     };
+  }, [todo, dialogRef, onClose]);
 
-    const timeAgo = formatDistanceToNow(todo.dateCreated,
-        {
-            addSuffix: false,
-            locale: enGB
-        });
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    updateTodo({ ...todo, description: todoDescription });
+    setShowDescriptionEditor(false);
+  };
 
-    const numberOfTimesMarkedAsToBeDoneToday = titleCase(converter.toWordsOrdinal(todo.numberOfTimesMarkedAsToBeDoneToday));
+  const timeAgo = formatDistanceToNow(todo.dateCreated, {
+    addSuffix: false,
+    locale: enGB,
+  });
 
-    return (
-        <dialog
-            ref={dialogRef}
-            className="fixed z-10 inset-0 overflow-y-auto bg-white w-full h-full"
-            aria-labelledby="modal-title"
-            role="dialog"
-            aria-modal="true">
+  const numberOfTimesMarkedAsToBeDoneToday = titleCase(
+    converter.toWordsOrdinal(todo.numberOfTimesMarkedAsToBeDoneToday),
+  );
 
+  return (
+    <dialog
+      ref={dialogRef}
+      className="fixed z-10 inset-0 overflow-y-auto bg-white w-full h-full"
+      aria-labelledby="modal-title"
+      role="dialog"
+      aria-modal="true"
+    >
+      <div className="flex items-center justify-end pt-6 pr-4">
+        <button
+          onClick={onClose}
+          className="border px-3 py-2 rounded hover:bg-gray-200"
+          title="Close (Esc)"
+        >
+          ✕<span className="sr-only">Close (Esc)</span>
+        </button>
+      </div>
+      <div className="flex flex-col px-8 py-4 gap-y-8">
+        <div className="flex flex-col gap-y-2 border-b max-w-md">
+          <p className="text-2xl self-start">{todo.title}</p>
+          {todo.numberOfTimesMarkedAsToBeDoneToday > 0 &&
+            filterType === "today" && (
+              <p className="text-sm text-gray-700">
+                <span>{numberOfTimesMarkedAsToBeDoneToday}</span> time in Today
+              </p>
+            )}
 
-            <div className="flex items-center justify-end pt-6 pr-4">
-                <button
-                    onClick={onClose}
-                    className="border px-3 py-2 rounded hover:bg-gray-200"
-                    title="Close (Esc)"
+          <p className="text-sm text-gray-700">{timeAgo} old</p>
+          <div className="flex space-x-4 py-2">
+            <MarkAsDoneButton
+              name="MarkAsDoneButton"
+              todoId={todo.id}
+              onClose={onClose}
+            />
+            <MoveToBacklogButton todoId={todo.id} onClose={onClose} />
+            <MoveToTodayButton todoId={todo.id} onClose={onClose} />
+
+            {!todo.dateDeleted && (
+              <SoftDeleteTodoButton todoId={todo.id} onClose={onClose} />
+            )}
+
+            {todo.dateDeleted && (
+              <HardDeleteSingleDeletedTodoButton
+                todoId={todo.id}
+                onClose={onClose}
+              />
+            )}
+          </div>
+        </div>
+
+        {!showDescriptionEditor && todo.description ? (
+          <>
+            <div className="flex flex-col gap-y-2">
+              <label
+                htmlFor="todoDescription"
+                className="flex flex-col text-sm text-gray-700"
+              >
+                Notes
+              </label>
+
+              <button
+                onClick={() => setShowDescriptionEditor(true)}
+                className="flex justify-start cursor-pointer rounded max-w-md"
+                id="todoDescription"
+              >
+                {todo.description}
+              </button>
+            </div>
+          </>
+        ) : (
+          <>
+            <form
+              className="flex flex-col gap-y-2 max-w-md w-full"
+              onSubmit={(event) => handleSubmit(event)}
+            >
+              <label
+                htmlFor="todoDescription"
+                className="flex flex-col text-sm"
+              >
+                Notes
+                <textarea
+                  name="todoDescription"
+                  id="todoDescription"
+                  rows={10}
+                  className="border rounded"
+                  value={todoDescription}
+                  onChange={(event) => setTodoDescription(event.target.value)}
                 >
-                    ✕
-                </button>
-            </div>
-            <div className="flex flex-col px-8 py-4 gap-y-8">
-                <div className="flex flex-col gap-y-2 border-b max-w-md">
-                    <p className="text-2xl self-start">
-                        {todo.title}
-                    </p>
-                    {
-                        todo.numberOfTimesMarkedAsToBeDoneToday > 0 && filterType === "today" &&
-                        <p className="text-sm text-gray-700">
-                            <span>{numberOfTimesMarkedAsToBeDoneToday}</span> time in Today
-                        </p>
-                    }
+                  {todo.description}
+                </textarea>
+              </label>
+              <button
+                type="submit"
+                className="border rounded px-2 py-1 hover:bg-gray-200 max-w-[200px]"
+              >
+                Save
+              </button>
+            </form>
+          </>
+        )}
 
-                    <p className="text-sm text-gray-700">
-                        {timeAgo} old
-                    </p>
-                    <div className="flex space-x-4 py-2">
-                        <MarkAsDoneButton name="MarkAsDoneButton" todoId={todo.id} onClose={onClose} />
-                        <MoveToBacklogButton todoId={todo.id} onClose={onClose} />
-                        <MoveToTodayButton todoId={todo.id} onClose={onClose} />
-
-                        {
-                            !todo.dateDeleted &&
-                            <SoftDeleteTodoButton todoId={todo.id} onClose={onClose} />
-                        }
-
-                        {
-                            todo.dateDeleted &&
-                            <HardDeleteSingleDeletedTodoButton todoId={todo.id} onClose={onClose} />
-                        }
-                    </div>
-                </div>
-
-                {
-                    (!showDescriptionEditor && todo.description) ? (
-                        <>
-                            <div className="flex flex-col gap-y-2">
-                                <label htmlFor="todoDescription" className="flex flex-col text-sm text-gray-700">
-                                    Notes
-                                </label>
-
-                                <button
-                                    onClick={() => setShowDescriptionEditor(true)}
-                                    className="flex justify-start cursor-pointer rounded max-w-md"
-                                    id="todoDescription"
-                                >
-                                    {todo.description}
-                                </button>
-                            </div>
-                        </>
-                    )
-                        :
-
-                        <>
-                            <form
-                                className="flex flex-col gap-y-2 max-w-md w-full"
-                                onSubmit={
-                                    (event) => handleSubmit(event)
-                                }
-                            >
-                                <label htmlFor="todoDescription" className="flex flex-col text-sm">
-                                    Notes
-                                    <textarea
-                                        name="todoDescription"
-                                        id="todoDescription"
-                                        rows={10}
-                                        className="border rounded"
-                                        value={todoDescription}
-                                        onChange={(event) => setTodoDescription(event.target.value)}
-
-                                    >
-                                        {todo.description}
-                                    </textarea>
-                                </label>
-                                <button type="submit" className="border rounded px-2 py-1 hover:bg-gray-200 max-w-[200px]">
-                                    Save
-                                </button>
-                            </form>
-                        </>
-                }
-
-
-
-
-                {
-                    todo.dateDeleted && (
-                        <p>
-                            Deleted: {todo.dateDeleted.toLocaleDateString()}
-                        </p>
-                    )
-                }
-
-            </div>
-
-        </dialog>
-    )
+        {todo.dateDeleted && (
+          <p>Deleted: {todo.dateDeleted.toLocaleDateString()}</p>
+        )}
+      </div>
+    </dialog>
+  );
 };
--- src/components/react/TodoList.tsx
+++ src/components/react/TodoList.tsx
@@ -2,198 +2,203 @@ import { useRef, useState } from "react";
 import type { FC } from "react";
 import { titleCase } from "title-case";
 import { useStore } from "@nanostores/react";
-import { formatDistanceToNow } from "date-fns"
+import { formatDistanceToNow } from "date-fns";
 import { enGB } from "date-fns/locale";
+import converter from "number-to-words";
 
-
-import { MarkAsDoneButton, SoftDeleteTodoButton, MoveToBacklogButton, MoveToTodayButton, HardDeleteAllDeletedTodosButton, SoftDeleteAllDoneTodosButton, HardDeleteSingleDeletedTodoButton, UnmarkAsDoneButton } from "./Buttons";
+import {
+  MarkAsDoneButton,
+  SoftDeleteTodoButton,
+  MoveToBacklogButton,
+  MoveToTodayButton,
+  HardDeleteAllDeletedTodosButton,
+  SoftDeleteAllDoneTodosButton,
+  HardDeleteSingleDeletedTodoButton,
+  UnmarkAsDoneButton,
+} from "./Buttons";
 import type { Todo } from "../../types/TodoType";
-import { $backlogTodos, $doneTodos, $todayTodos, $filterType, $allTodos, $deletedTodos } from "../../stores/store";
+import {
+  $backlogTodos,
+  $doneTodos,
+  $todayTodos,
+  $filterType,
+  $allTodos,
+  $deletedTodos,
+} from "../../stores/store";
 import { TodoDialog } from "./TodoDialog";
 
 export const TodoActionButtons: FC<{ todo: Todo }> = ({ todo }) => {
-    const filterType = useStore($filterType);
-    const todoId = todo.id;
+  const filterType = useStore($filterType);
+  const todoId = todo.id;
 
-    const buttonMap = {
-        "done": [
-            <SoftDeleteTodoButton todoId={todoId} />,
-            <MoveToBacklogButton todoId={todoId} />,
-            <MoveToTodayButton todoId={todoId} />
-        ],
-        "deleted": [
-            <MoveToBacklogButton todoId={todoId} />,
-            <MoveToTodayButton todoId={todoId} />,
-            <HardDeleteSingleDeletedTodoButton todoId={todoId} />
-        ],
-        "backlog": [
-            <MarkAsDoneButton name="MarkAsDoneButton" todoId={todoId} />,
-            <MoveToTodayButton todoId={todoId} />,
-            <SoftDeleteTodoButton todoId={todoId} />
-        ],
-        "today": [
-            <UnmarkAsDoneButton name="UnmarkAsDoneButton" todoId={todoId} />,
-            <MarkAsDoneButton name="MarkAsDoneButton" todoId={todoId} />,
-            <MoveToBacklogButton todoId={todoId} />,
-            <SoftDeleteTodoButton todoId={todoId} />
-        ],
-        "all": [
-            <UnmarkAsDoneButton name="UnmarkAsDoneButton" todoId={todoId} />,
-            <MarkAsDoneButton name="MarkAsDoneButton" todoId={todoId} />,
-            <MoveToBacklogButton todoId={todoId} />,
-            <MoveToTodayButton todoId={todoId} />,
-            <SoftDeleteTodoButton todoId={todoId} />
-        ],
-    };
+  const buttonMap = {
+    done: [
+      <SoftDeleteTodoButton todoId={todoId} />,
+      <MoveToBacklogButton todoId={todoId} />,
+      <MoveToTodayButton todoId={todoId} />,
+    ],
+    deleted: [
+      <MoveToBacklogButton todoId={todoId} />,
+      <MoveToTodayButton todoId={todoId} />,
+      <HardDeleteSingleDeletedTodoButton todoId={todoId} />,
+    ],
+    backlog: [
+      <MarkAsDoneButton name="MarkAsDoneButton" todoId={todoId} />,
+      <MoveToTodayButton todoId={todoId} />,
+      <SoftDeleteTodoButton todoId={todoId} />,
+    ],
+    today: [
+      <UnmarkAsDoneButton name="UnmarkAsDoneButton" todoId={todoId} />,
+      <MarkAsDoneButton name="MarkAsDoneButton" todoId={todoId} />,
+      <MoveToBacklogButton todoId={todoId} />,
+      <SoftDeleteTodoButton todoId={todoId} />,
+    ],
+    all: [
+      <UnmarkAsDoneButton name="UnmarkAsDoneButton" todoId={todoId} />,
+      <MarkAsDoneButton name="MarkAsDoneButton" todoId={todoId} />,
+      <MoveToBacklogButton todoId={todoId} />,
+      <MoveToTodayButton todoId={todoId} />,
+      <SoftDeleteTodoButton todoId={todoId} />,
+    ],
+  };
 
-    const buttons = buttonMap[filterType] || [];
+  const buttons = buttonMap[filterType] || [];
 
-    return (
-        <>
-            {buttons.map((button, index) => {
-                if (button.props.name === "MarkAsDoneButton" && todo.isDone) {
-                    return null;
-                }
+  return (
+    <>
+      {buttons.map((button, index) => {
+        if (button.props.name === "MarkAsDoneButton" && todo.isDone) {
+          return null;
+        }
 
-                if (button.props.name === "UnmarkAsDoneButton" && !todo.isDone) {
-                    return null;
-                }
+        if (button.props.name === "UnmarkAsDoneButton" && !todo.isDone) {
+          return null;
+        }
 
-                return (
-                    <span key={index}>
-                        {button}
-                    </span>
-                )
-            }
-
-            )}
-        </>
-    );
+        return <span key={index}>{button}</span>;
+      })}
+    </>
+  );
 };
 
+const TodoItem: FC<{
+  todo: Todo;
+  openDialogWithTodo: (todo: Todo) => void;
+}> = ({ todo, openDialogWithTodo }) => {
+  const todoTitleElement = todo.isDone ? <s>{todo.title}</s> : todo.title;
 
-const TodoItem: FC<{ todo: Todo, openDialogWithTodo: (todo: Todo) => void }> = ({ todo, openDialogWithTodo }) => {
+  const timeAgo = formatDistanceToNow(todo.dateCreated, {
+    addSuffix: false,
+    locale: enGB,
+  });
 
-    const todoTitleElement = todo.isDone ? <s>{todo.title}</s> : todo.title;
+  const numberOfTimesMarkedAsToBeDoneToday = titleCase(
+    converter.toWordsOrdinal(todo.numberOfTimesMarkedAsToBeDoneToday),
+  );
 
-    const timeAgo = formatDistanceToNow(todo.dateCreated,
-        {
-            addSuffix: false,
-            locale: enGB
-        });
+  return (
+    <li key={todo.id} className="flex gap-x-4 items-center justify-between">
+      <div className="w-full border">
+        <button
+          onClick={() => openDialogWithTodo(todo)}
+          className={`flex flex-col gap-y-4 w-full justify-start ${
+            todo.isDone ? "line-through" : ""
+          }`}
+        >
+          {todoTitleElement}
 
-    return (
-        <li key={todo.id} className="flex gap-x-4 items-center justify-between">
-            <div className="w-full border">
-                <button onClick={() => openDialogWithTodo(todo)} className={`flex flex-col gap-y-4 w-full justify-start ${todo.isDone ? "line-through" : ""}`}>
-                    {todoTitleElement}
+          {todo.numberOfTimesMarkedAsToBeDoneToday > 0 && (
+            <span className="text-xs">
+              {numberOfTimesMarkedAsToBeDoneToday} time in Today
+            </span>
+          )}
 
-                    {
-                        todo.numberOfTimesMarkedAsToBeDoneToday > 0 &&
-                        <span className="text-xs">
-                            {todo.numberOfTimesMarkedAsToBeDoneToday} times marked as to be done today
-                        </span>
-                    }
-
-                    {
-                        !todo.isDone &&
-
-                        <span className="text-xs">
-                            {timeAgo} old
-                        </span>
-                    }
-                </button>
-
-            </div>
-            <div className="flex gap-x-4">
-                <TodoActionButtons todo={todo} />
-            </div>
-        </li>
-    );
+          {!todo.isDone && <span className="text-xs">{timeAgo} old</span>}
+        </button>
+      </div>
+      <div className="flex gap-x-4">
+        <TodoActionButtons todo={todo} />
+      </div>
+    </li>
+  );
 };
 
 export const TodoList: FC = () => {
+  const filterType = useStore($filterType);
+  const allTodos = useStore($allTodos);
+  const todayTodos = useStore($todayTodos);
+  const backlogTodos = useStore($backlogTodos);
+  const doneTodos = useStore($doneTodos);
+  const deletedTodos = useStore($deletedTodos);
 
-    const filterType = useStore($filterType);
-    const allTodos = useStore($allTodos);
-    const todayTodos = useStore($todayTodos);
-    const backlogTodos = useStore($backlogTodos);
-    const doneTodos = useStore($doneTodos);
-    const deletedTodos = useStore($deletedTodos);
+  let todos: Todo[] = [];
 
+  if (filterType === "today") {
+    todos = todayTodos;
+  } else if (filterType === "all") {
+    todos = allTodos;
+  } else if (filterType === "backlog") {
+    todos = backlogTodos;
+  } else if (filterType === "done") {
+    todos = doneTodos;
+  } else if (filterType === "deleted") {
+    todos = deletedTodos;
+  }
 
-    let todos: Todo[] = [];
+  // Logging out the current filter"s todos
+  console.log(todos);
 
-    if (filterType === "today") {
-        todos = todayTodos;
-    } else if (filterType === "all") {
-        todos = allTodos;
-    } else if (filterType === "backlog") {
-        todos = backlogTodos;
-    } else if (filterType === "done") {
-        todos = doneTodos;
+  const dialogRef = useRef<HTMLDialogElement>(null);
+
+  const [selectedTodo, setSelectedTodo] = useState<Todo | null>(null);
+
+  const openDialogWithTodo = (todo: Todo) => {
+    setSelectedTodo(todo);
+  };
+
+  const closeDialog = () => {
+    if (dialogRef.current) {
+      dialogRef.current.close();
     }
-    else if (filterType === "deleted") {
-        todos = deletedTodos;
-    }
+    setSelectedTodo(null);
+  };
 
+  return (
+    <>
+      {selectedTodo && (
+        <TodoDialog
+          todo={selectedTodo}
+          onClose={closeDialog}
+          dialogRef={dialogRef}
+        />
+      )}
 
-    // Logging out the current filter"s todos
-    console.log(todos);
+      <div className="flex flex-col gap-y-8">
+        <h2 className="text-2xl font-semibold pb-1 border-b max-w-xs">
+          {titleCase(filterType)}
+        </h2>
 
-    const dialogRef = useRef<HTMLDialogElement>(null);
+        {todos.length === 0 && <p>Nothing here</p>}
 
-    const [selectedTodo, setSelectedTodo] = useState<Todo | null>(null);
+        <ul className="flex flex-col gap-y-12 max-w-3xl">
+          {todos.map((todo) => (
+            <TodoItem
+              key={todo.id}
+              todo={todo}
+              openDialogWithTodo={openDialogWithTodo}
+            />
+          ))}
+        </ul>
 
-    const openDialogWithTodo = (todo: Todo) => {
-        setSelectedTodo(todo);
-    };
+        {(filterType === "done" ||
+          (filterType === "all" && doneTodos.length > 0)) && (
+          <SoftDeleteAllDoneTodosButton />
+        )}
 
-    const closeDialog = () => {
-        if (dialogRef.current) {
-            dialogRef.current.close();
-        }
-        setSelectedTodo(null);
-    };
-
-    return (
-        <>
-            {selectedTodo &&
-
-                <TodoDialog todo={selectedTodo} onClose={closeDialog} dialogRef={dialogRef} />
-
-            }
-
-            <div className="flex flex-col gap-y-8">
-
-                <h2 className="text-2xl font-semibold pb-1 border-b max-w-xs">
-                    {titleCase(filterType)}
-                </h2>
-
-                {
-                    todos.length === 0 &&
-                    <p>
-                        Nothing here
-                    </p>
-
-                }
-
-
-
-                <ul className="flex flex-col gap-y-12 max-w-3xl">
-                    {todos.map((todo) => (
-                        <TodoItem key={todo.id} todo={todo} openDialogWithTodo={openDialogWithTodo} />
-                    ))}
-                </ul>
-
-
-
-                {(filterType === "done" || filterType === "all" && doneTodos.length > 0) && <SoftDeleteAllDoneTodosButton />}
-
-                {filterType === "deleted" && deletedTodos.length > 0 &&
-                    <HardDeleteAllDeletedTodosButton />}
-
-            </div>
-        </>
-    );
+        {filterType === "deleted" && deletedTodos.length > 0 && (
+          <HardDeleteAllDeletedTodosButton />
+        )}
+      </div>
+    </>
+  );
 };
