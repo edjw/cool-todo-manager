d901dc47308d474402aa41e5ce31bea2325c44a0
Ed Johnson-Williams <git@johnsonwilliams.co.uk>
2023-10-10 15:45:49 +0100

    few more changes to input for mobile

--- src/components/react/TodoInput.tsx
+++ src/components/react/TodoInput.tsx
@@ -41,7 +41,7 @@ export const TodoInput: FC = () => {
 
   return (
     <form
-      className="flex flex-col sm:flex-row justify-between items-start gap-x-8 max-w-xl"
+      className="flex flex-col sm:flex-row justify-between items-start gap-x-8 gap-y-4 max-w-xl"
       onSubmit={handleSubmit}
     >
       <div className="flex flex-col sm:flex-row flex-wrap gap-x-4 gap-y-4">
@@ -49,10 +49,10 @@ export const TodoInput: FC = () => {
           value={inputValue}
           placeholder="Add a todo"
           onChange={(event) => setInputValue(event.target.value)}
-          className="border px-4 py-2 rounded-lg sm:min-w-[400px]"
+          className="border px-4 py-2 rounded-lg w-[300px] sm:min-w-[400px]"
         ></textarea>
 
-        <div className="flex items-center gap-x-4 relative group focus-within:ring-4 focus-within:ring-blue-500 rounded-lg">
+        <div className="flex items-center gap-x-4 relative group focus-within:ring-4 focus-within:ring-blue-500 rounded-lg w-fit">
           <label
             htmlFor="today"
             title="Add to today's todos"
