41f78aa14bb6ec4bb31ad3fc37fc1a8c999a9b70
Ed Johnson-Williams <git@johnsonwilliams.co.uk>
2023-10-11 12:13:20 +0100

    add service worker for offlien

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
+import serviceWorker from "astrojs-service-worker";
 // import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()],
+  integrations: [react(), tailwind(), serviceWorker()],
   output: "static",
   // adapter: adapter(),
 });
