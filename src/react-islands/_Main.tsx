23b462265c6b621523b90d9c1399a4626bf534a8
Ed Johnson-Williams <git@johnsonwilliams.co.uk>
2023-10-12 15:00:59 +0100

    revert error

--- src/react-islands/_Main.tsx
+++ src/react-islands/_Main.tsx
@@ -10,12 +10,11 @@ const Main = () => {
   return (
     <>
       <main
-        className={`grid grid-cols-1 sm:grid-rows-[auto_1fr] h-full gap-x-20 gap-y-8 relative`}
+        className={`grid grid-cols-1 sm:grid-cols-[auto_1fr] h-full gap-x-20 gap-y-8 relative`}
       >
         <aside className={`flex flex-col gap-y-8`}>
           <ListSwitcher />
         </aside>
-
         <div className="grid grid-cols-1 grid-rows-[auto_1fr] gap-y-12">
           <TodoInput />
           <TodoList />
